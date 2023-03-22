3a8bdb97ab69b60afc6c4f4bca474d1b118385f4
Shashikant120 <128563439+Shashikant120@users.noreply.github.com>
2023-03-22 09:08:34 +0530

    Files added properly

--- helloworld.java
+++ helloworld.java
@@ -1,16 +1,15 @@
-class Main{
-  public static void main (string []args){
-    for(int i=0; i<=10;i++){
-      System.out.println("Hello World");
-    }
-  }
-}
-
-
 // Using while loop
+
 System.out.println("Using while loops");
 int j =0;
 while(j!=10){
 System.out.println("Hello World");
   j++;
 }
+
+//using do while loop
+
+do{
+System.out.println("Hello World");
+  j++;
+} while(j!=20);
