94eb4b35c76d53e02f0849c21ea7f023c541c878
Shashikant120 <128563439+Shashikant120@users.noreply.github.com>
2023-03-22 09:11:05 +0530

    While loop added in main class

--- helloworld.java
+++ helloworld.java
@@ -3,14 +3,18 @@ class Main{
     for(int i=0; i<=10;i++){
       System.out.println("Hello World");
     }
-  }
-}
-
-
-// Using while loop
+    // Using while loop
 System.out.println("Using while loops");
 int j =0;
 while(j!=10){
 System.out.println("Hello World");
   j++;
 }
+  }
+}
+
+
+
+
+
+
