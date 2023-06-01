a7694342bb1f55689f97f895050e58925a8050a1
Shashikant120 <128563439+Shashikant120@users.noreply.github.com>
2023-06-01 12:58:37 +0530

    With added edit button
    this is assignment on sharpener where we need to create delete element and with added edit button it has been re uploaded

--- code.js
+++ code.js
@@ -1,42 +1,40 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+let get=document.querySelector("#addForm");
+get.addEventListener("submit",addelements);
 
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
+let place=document.querySelector("#items");
 
-
-function addItem(e){
-  e.preventDefault();
-
- 
-  var newItem = document.getElementById('item').value;
-
-  var li = document.createElement('li');
-
-  li.className = 'list-group-item';
-
-  li.appendChild(document.createTextNode(newItem));
-
-  var deleteBtn = document.createElement('button');
-
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  li.appendChild(deleteBtn);
-
-  itemList.appendChild(li);
+//remove element
+place.addEventListener('click',removelement);
+function addelements(e){
+    e.preventDefault();
+    let value=document.querySelector("#item").value;
+    let create=document.createElement("li");
+    create.classList="li class";
+    create.id="li id";
+    let textnode1= document.createTextNode(value);
+    create.appendChild(textnode1);
+    let button=document.createElement("button");
+    let textnode2 = document.createTextNode('X');
+    button.classList="btn btn-danger btn-sm float-right delete";
+    button.appendChild(textnode2);
+    create.appendChild(button);
+    place.appendChild(create);
+    let editbutton=document.createElement("button");
+    editbutton.classList="btn btn-danger btn-sm float-right delete";
+    let textnode3=document.createTextNode("Edit");
+    editbutton.appendChild(textnode3);
+    create.appendChild(editbutton);
+    create.appendChild(editbutton);
 }
+function removelement(e){
+    e.preventDefault();
+   if( e.target.classList.contains("delete")){
+        if(confirm('do you really wants to remove this item ?')){
+            let targeted=e.target.parentNode;
+            place.removeChild(targeted);
 
-
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
+        }
     }
-  }
+
 }
