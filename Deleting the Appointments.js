e5159461ace7537f8ea734baa49e957fdec5195a
Shashikant120 <128563439+Shashikant120@users.noreply.github.com>
2023-07-12 21:56:46 +0530

    Deleting the Appointments

---
+++ Deleting the Appointments.js
@@ -0,0 +1,57 @@
+let form=document.getElementById('form');
+form.addEventListener('submit',storeuser);
+function storeuser(e){
+e.preventDefault();
+let email=document.getElementById('email').value;
+let name=document.getElementById('name').value;
+let obj={
+    name:name,
+    email:email,
+};
+let ul=document.getElementById('items');
+let li=document.createElement('li');
+li.appendChild(document.createTextNode(`${name}-${email}`));
+let input=document.createElement('input');
+input.type="button";
+input.value="delete";
+li.appendChild(input);
+ul.appendChild(li);
+
+let convert=JSON.stringify(obj);
+localStorage.setItem(email,convert);
+axios.post('https://crudcrud.com/api/1db55c7d0eff4446862b6ceb9ebb4408/unicorns',obj).then((data)=>{
+  
+}).catch((error)=>{
+    console.log(error);
+})
+input.onclick=(e)=>{
+input.parentElement.remove();
+localStorage.removeItem(email);
+axios.get('https://crudcrud.com/api/1db55c7d0eff4446862b6ceb9ebb4408/unicorns').then((response)=>{
+    for(let i=0;i<response.data.length;i++){
+        if(response.data[i].email==email){
+            let id=response.data[i]._id;
+            axios.delete('https://crudcrud.com/api/1db55c7d0eff4446862b6ceb9ebb4408/unicorns/'+id);
+           
+        }
+    }
+})
+}
+};
+window.addEventListener('DOMContentLoaded',showdata);   
+function showdata(e){
+    e.preventDefault();
+    axios.get('https://crudcrud.com/api/1db55c7d0eff4446862b6ceb9ebb4408/unicorns').then((response)=>{
+        for(let i=0;i<response.data.length;i++){
+            let name=response.data[i].name;
+            let email=response.data[i].email;
+            let ul=document.getElementById('items');
+         let li=document.createElement('li');
+         li.appendChild(document.createTextNode(`${name}-${email}`));
+         ul.appendChild(li);
+
+        }
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
